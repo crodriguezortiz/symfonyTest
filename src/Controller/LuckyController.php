9d4aaecd1ac686bf977afc6a9ab4015f39cc8f4f
crodriguezortiz <carlos.rodriguez.otz@gmail.com>
2018-12-18 12:02:06 +0100

    change routes in route.yaml for annotantions

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,10 +2,14 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class LuckyController
 {
+   /**
+      * @Route("/lucky/number")
+      */
     public function number()
     {
         $number = random_int(0, 100);
