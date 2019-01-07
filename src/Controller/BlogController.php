bdc5019b9ea829690d08059009addcef70604fc9
crodriguezortiz <carlos.rodriguez.otz@gmail.com>
2019-01-07 11:27:02 +0100

    Routing start

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,31 @@
+// src/Controller/BlogController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * Matches /blog exactly
+     *
+     * @Route("/blog", name="blog_list")
+     */
+    public function list()
+    {
+        // ...
+    }
+
+    /**
+     * Matches /blog/*
+     *
+     * @Route("/blog/{slug}", name="blog_show")
+     */
+    public function show($slug)
+    {
+        // $slug will equal the dynamic part of the URL
+        // e.g. at /blog/yay-routing, then $slug='yay-routing'
+
+        // ...
+    }
+}
---
+++ src/Controller/CompanyController.php
@@ -0,0 +1,19 @@
+// src/Controller/CompanyController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CompanyController extends AbstractController
+{
+    /**
+     * @Route({
+     *     "nl": "/over-ons",
+     *     "en": "/about-us"
+     * }, name="about_us")
+     */
+    public function about()
+    {
+        // ...
+    }
+}
